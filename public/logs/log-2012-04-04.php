30881a7d510655433881539f1fed2ff86eca6b7f
ifind jcs <weifind@qq.com>
2012-04-07 12:44:05 +0800

    记录日志的样式

---
+++ public/logs/log-2012-04-04.php
@@ -0,0 +1,13 @@
+<?php  if (! defined ( 'PROJECT_DIR' )) exit ( 'No direct script access allowed' );?>
+
+ERROR - 2012-04-04 18:15:23 --> Severity: Warning  --> include(/var/www/frame/cgi-bin/ifind/TesttwoIfind.php): failed to open stream: No such file or directory /var/www/frame/index.php 67
+ERROR - 2012-04-04 18:15:23 --> Severity: Warning  --> include(): Failed opening '/var/www/frame/cgi-bin/ifind/TesttwoIfind.php' for inclusion (include_path='.:/usr/local/php/lib/php') /var/www/frame/index.php 67
+ERROR - 2012-04-04 18:18:37 --> Severity: Warning  --> include(/var/www/frame/cgi-bin/ifind/TesttwoIfind.php): failed to open stream: No such file or directory /var/www/frame/index.php 67
+ERROR - 2012-04-04 18:22:38 --> Severity: Warning  --> include(/var/www/frame/cgi-bin/ifind/TesttwoIfind.php): failed to open stream: No such file or directory /var/www/frame/index.php 67
+ERROR - 2012-04-04 18:22:38 --> 404 Page Not Found --> 
+ERROR - 2012-04-04 18:26:07 --> Severity: Warning  --> include(/var/www/frame/cgi-bin/ifind/TesttwoIfind.php): failed to open stream: No such file or directory /var/www/frame/index.php 67
+ERROR - 2012-04-04 18:26:07 --> 404 Page Not Found --> 
+ERROR - 2012-04-04 18:27:57 --> Severity: Warning  --> include(/var/www/frame/cgi-bin/ifind/TesttwoIfind.php): failed to open stream: No such file or directory /var/www/frame/index.php 67
+ERROR - 2012-04-04 18:27:57 --> 404 Page Not Found --> 
+ERROR - 2012-04-04 18:54:57 --> Severity: Warning  --> include(/var/www/frame/cgi-bin/ifind/TesttwoIfind.php): failed to open stream: No such file or directory /var/www/frame/index.php 69
+ERROR - 2012-04-04 18:54:57 --> 404 Page Not Found --> 
---
+++ public/logs/log-2012-04-05.php
@@ -0,0 +1,11 @@
+<?php  if (! defined ( 'PROJECT_DIR' )) exit ( 'No direct script access allowed' );?>
+
+DEBUG - 2012-04-05 17:46:46 --> Security Class Initialized
+DEBUG - 2012-04-05 17:48:00 --> Security Class Initialized
+DEBUG - 2012-04-05 17:49:39 --> Security Class Initialized
+DEBUG - 2012-04-05 17:49:39 --> XSS Filtering completed
+DEBUG - 2012-04-05 17:50:52 --> Security Class Initialized
+ERROR - 2012-04-05 17:50:52 --> Severity: Warning  --> preg_replace(): No ending matching delimiter '&gt;' found /var/www/frame/cgi-bin/libs/SecurityClass.php 306
+ERROR - 2012-04-05 17:50:52 --> 404 Page Not Found --> 
+DEBUG - 2012-04-05 17:51:33 --> Security Class Initialized
+DEBUG - 2012-04-05 17:51:33 --> XSS Filtering completed
