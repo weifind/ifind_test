d13b1aa4f4e46b0f16635b5b0f934206aed65c7c
ifind jcs <weifind@qq.com>
2012-04-07 12:42:51 +0800

    视图示例，以及错误模板

---
+++ view/error/template.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<title>there is no page</title>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+</head>
+<body style="margin:0; background-image: url(/404-1.gif);">
+<table width="563" border="0" align="center" cellpadding="0" cellspacing="0">
+  <tr>
+    <td height="125">&nbsp;</td>
+  </tr>
+  <tr>
+    <td style="height:195;bgcolor:#FFFFFF;background:/1.gif;">
+      <table border="0" align="center" cellpadding="0" cellspacing="0">
+        <tr>
+          <td width="210"><a href="#"><img src="/404-2.gif" alt="" width="210" height="80" border="0" /></a></td>
+          <td style="color:#333333; font-size:14px; font-weight:bold;">页面没有找到，5秒钟之后将带您进入首页！</td>
+        </tr>
+      </table>
+    </td>
+  </tr>
+</table>
+<script>
+function jump()
+{
+        if(document.title=="there is no page")location.href = "index.php";
+}
+setTimeout("jump()", 5000);
+</script>
+</body>
+</html>
